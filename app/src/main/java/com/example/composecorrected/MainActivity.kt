1abcc981f80581ab71f0fe271fe7c50d17afaa06
shilohcoconuts <shilohpalmer57@gmail.com>
2024-12-05 14:35:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/composecorrected/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.composecorrected
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.composecorrected.ui.theme.ComposeCorrectedTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeCorrectedTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Counter()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Counter() {
+var counter by rememberSaveable { mutableIntStateOf(0)}
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+
+        Button (
+            onClick = {counter++}
+        ){
+            Text("Increase")
+        }
+        Text(
+            text = counter.toString(),
+            fontSize = 64.sp
+
+        )
+
+        Button(
+            onClick = {counter--}
+        ){
+            Text("Decrease")
+        }
+    }
+
+
+}
+//for color in colors Text(color)
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeCorrectedTheme {
+        Counter()  }
+}
